a67a7ba11875411bb447f11346319afedaa644bc
jeisenbers <anna.lima@senior-rp.com.br>
2025-08-01 19:47:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def serve_index():
-    return send_from_directory('.', 'index.html')
+def login():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
